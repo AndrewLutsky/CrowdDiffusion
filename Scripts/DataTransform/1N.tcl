a6b9d5a73621bf59069fecc9e3cfa7f769b8b49d
AndrewLutsky <andrew.lutsky19@gmail.com>
2023-02-23 18:19:01 -0600

    created documentation

--- Scripts/DataTransform/1N.tcl
+++ Scripts/DataTransform/1N.tcl
@@ -5,31 +5,37 @@
 #a selection then performs fits of the protein over all frames and 
 #uses the transformation matrix to transform all other coordinates
 
-#read in pdb and psf files
-#read in dcd files
 
+#gets the current path
 set path [pwd]
 puts $path
 cd $path
 
 
 
-
+#imports qwrap package
 package require qwrap
+#gets the psfname, pdbname, number of runs, and the name of the trajectory file from arguments
 set psfname [lindex $argv 0]
 set pdbname [lindex $argv 0]
 set numberTraj [lindex $argv 1]
 set trajfile [lindex $argv 2]
 
-mol new /Scr/alutsky/scripts/DataTransform/ref3.pdb waitfor all
+#file to align to 
+mol new ~/Desktop/CrowdDiffusion/ref3.pdb waitfor all
+#sets the reference and the list of unique residues in this pdb
 set reference [atomselect top all]
 set residList [$reference get resid]
 set uniqResList [lsort -unique $residList]
 
+#gets the selection of unique residues to align to the reference pdb
+set segsel "segname AP1 and resid $uniqResList"
+
+#adds a new copy of the psf and pdb and outputs the run number
 mol new $psfname.psf waitfor all
 mol addfile $pdbname.pdb waitfor all
 puts $trajfile
-set segsel "segname AP1 and resid $uniqResList"
+#adds each trajectory file and waits for them all to load in
 for {set i 1} {$i <= $numberTraj} {incr i} {
 	mol addfile $trajfile$i.dcd waitfor all
 }
@@ -39,49 +45,65 @@ for {set i 1} {$i <= $numberTraj} {incr i} {
 package require qwrap
 qunwrap
 
-#detet if ion present
+#detect if the ion is present or not(which position getFile to call)
 set magPresent [expr {[ [atomselect top "name MG"] num] > 0}]
 set sodPresent [expr {[ [atomselect top "name SOD"] num] > 0}]
 set claPresent [expr {[ [atomselect top "name CLA"] num] > 0}]
 
 #sets arguments for Ion writeout
 set argv [list $pdbname-unwrapped 0]
+#ion writeout
 if {$magPresent == 1} {
-	source /Scr/alutsky/scripts/Mag/getPosMag.tcl
+	source ~/Desktop/CrowdDiffusion/Scripts/getFiles/Mag/getPosMag.tcl
 }
 if {$sodPresent == 1} {
-	source /Scr/alutsky/scripts/Sod/getPosSod.tcl
+	source ~/Desktop/CrowdDiffusion/Scripts/getFiles/Sod/getPosSod.tcl
 }
 if {$claPresent == 1} {
-	source /Scr/alutsky/scripts/Cla/getPosCla.tcl
+	source ~/Desktop/CrowdDiffusion/Scripts/getFiles/Cla/getPosCla.tcl
 }
-source /Scr/alutsky/scripts/ATP/getPosATP1N.tcl
+
+#call the ATP get file
+source ~/Desktop/CrowdDiffusion/Scripts/ATP/getPosATP1N.tcl
+#rewrap the file so that the protein is centered
 qwrap sel "not segname AP1" center "segname AP1"
 puts "setting numframes"
 
 
-
+#set the number of frames
 set numframes [molinfo top get numframes]
+#create the selection using the previous list of unique reference residues
 set sel [atomselect top $segsel]
 
+
 puts "For Loop engage"
+#creates a selection of all atoms using name all
 set all [atomselect top all]
+#loops through each frame
 for {set i 1} {$i < $numframes} {incr i} {
+	#updated segname AP1(protein selections) to frame i
 	$sel frame $i
+	#create a rotation and translation matrix using the reference and the protein selection
 	set transmat [measure fit $sel $reference]
-	$all frame $i	
+	#update all
+	$all frame $i
+	#move all atoms using the transformation matrix(rotates and translates)
 	$all move $transmat
 }
 
+#sets the argument to be the name of the pdb file
 set argv [list $pdbname-wrapped 0]
 
+#writes wrapped positions of ions
 if {$magPresent == 1} {
-	source /Scr/alutsky/scripts/Mag/getPosMag.tcl
+	source ~/Desktop/CrowdDiffusion/Scripts/getFiles/Mag/getPosMag.tcl
 }
 if {$sodPresent == 1} {
-	source /Scr/alutsky/scripts/Sod/getPosSod.tcl
+	source ~/Desktop/CrowdDiffusion/Scripts/getFiles/Sod/getPosSod.tcl
 }
 if {$claPresent == 1} {
-	source /Scr/alutsky/scripts/Cla/getPosCla.tcl
+	source ~/Desktop/CrowdDiffusion/Scripts/getFiles/Cla/getPosCla.tcl
 }
-source /Scr/alutsky/scripts/ATP/getPosATP1N.tcl
+
+#call the ATP get file
+source ~/Desktop/CrowdDiffusion/Scripts/ATP/getPosATP1N.tcl
--- Scripts/getFiles/ATP/getPosATP2N.tcl
+++ Scripts/getFiles/ATP/getPosATP2N.tcl
@@ -1,41 +1,68 @@
 package require pbctools
 
+#prints writing ATP
 puts "writing ATP"
 
+
+#selector for ATP segments
 set ATP [atomselect top "segname TP1 to TP9 TQ0 to TQ9"]
+
+#sets number of frames and path to a variable
 set nf [molinfo top get numframes]
 set pathDir [pwd]
 
+#prints first argument
 puts [lindex $argv 0]
-cd /Scr/alutsky/scripts/Analysis/
+
+#changes directory to output directory
+cd ~/Desktop/CrowdDiffusion/
+
+#sets name and count of how many runs
 set nameSim [lindex $argv 0]
 set count [lindex $argv 1]
+#creates a directory after name of Sim and changes to that directory
 file mkdir "$nameSim"
 cd "$nameSim"
 
-#writes the Cla selection to a position file format which stores all positions
+#writes the ATP selection to a position file format which stores all positions
 
+
+#opens file
 set fo [open "$nameSim-$count-ATP.csv" w ]
 
+#puts header
 puts $fo "Frame Number,Index,X,Y,Z"
+#loops over each frame
 for { set f 0} {$f <= $nf} {incr f} {
+	#Updates ATP selection to frame f
 	$ATP frame $f
 	$ATP update
-
+	
+	#gets a list of all atoms in ATP selection in frame f
 	set ATPPos [$ATP get {x y z segname} ]
+	#gets length of the list
 	set ATPLength [llength $ATPPos]
+
+	#loops through each index of the list
 	for {set i 0} {$i < $ATPLength} {incr i} {
 		
+		#gets the index
 		set ATPPosIndex [lindex $ATPPos $i]
-
+		#gets X,Y, and Z
 		set ATPx [lindex $ATPPosIndex 0]
 		set ATPy [lindex $ATPPosIndex 1]
 		set ATPz [lindex $ATPPosIndex 2]
+		#gets ATP segment name
 		set ATPseg [lindex $ATPPosIndex 3]
+
+		#outputs in order of frame number, atom index, x, y, z, and segment
 		puts $fo "$f,$i,$ATPx,$ATPy,$ATPz,$ATPseg" 	
 	}
 }	
 
+#closes the file
 close $fo
 
+
+#goes to original directory
 cd "$pathDir"
